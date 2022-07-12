98864f58111e7f6b37ad5f04475ea71f880257e2
= <=>
2022-07-12 08:46:53 -0400

    I still haven't found what I'm looking for.

--- dub-dub-dub.py
+++ dub-dub-dub.py
@@ -1,25 +1,4 @@
-from google import search
+import pywhatkit as kt
 
-query = "geeksteev"
-
-for i in search(query=query,tld='co.in',lang='en',num=10,stop=1,pause=2):
-    print(i +'\n')
-
-
-# # Performing google search using Python code
-# class Gsearch_python:
-#    def __init__(self,name_search):
-#       self.name = name_search
-#    def Gsearch(self):
-#       count = 0
-#       try :
-#          from google import search
-#       except ImportError:
-#          print("No Module named 'google' Found")
-#       for i in search(query=self.name,tld='co.in',lang='en',num=10,stop=1,pause=2):
-#          count += 1
-#          print (count)
-#          print(i + '\n')
-# if __name__=='__main__':
-#    gs = Gsearch_python("Tutorialspoint Python")
-#    gs.Gsearch()
+query = 'the new hotness'
+kt.search(query)
